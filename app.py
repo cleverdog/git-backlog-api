45084e5c3278961b36ace6e89b572b8817ddee32
cleverdog <motoki@cleverdog.me>
2020-01-04 16:04:48 +0900

    test 3

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def post_json():
     branch_name = 'feature/' + project_key + '_' + cat + '_' + str(key_id)
     _repo_path = os.path.join('./', 'repo')
     # clone from remote
-    # git_repo = git.Repo.clone_from('gitclb@gitclb.git.backlog.com:/TEST/test.git', _repo_path, branch='master')
+    git_repo = git.Repo.clone_from('gitclb@gitclb.git.backlog.com:/TEST/test.git', _repo_path, branch='master')
     # create future branch
     # origin = git_repo.remote()
     # git_repo.create_head(branch_name)
