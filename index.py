d4ae1f9f8b03e3207cc7744673c4a0d593ece15a
cleverdog <motoki@cleverdog.me>
2020-01-04 18:22:37 +0900

    test 13

--- index.py
+++ index.py
@@ -2,12 +2,12 @@ import os
 import git
 
 # SSH鍵の書き出し
-with open('./id_rsa.pub', 'w') as f:
+with open('./id_rsa', 'w') as f:
     f.write('<SSH Key>')
-    os.chmod('./id_rsa.pub', 0o600)
+    os.chmod('./id_rsa', 0o600)
 # SSHコマンドの書き出し
 with open('./ssh_cmd', 'w') as f:
-    f.write('ssh -i ../id_rsa.pub -oIdentitiesOnly=yes "$@"')
+    f.write('ssh -i ../id_rsa -oIdentitiesOnly=yes "$@"')
     os.chmod('./ssh_cmd', 0o777)
 
 _repo_path = os.path.join('./', 'repo')
