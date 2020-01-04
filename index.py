ebf264455f431719a7cf06f6ce5b074a06f3884f
cleverdog <motoki@cleverdog.me>
2020-01-04 18:19:22 +0900

    test 12

--- index.py
+++ index.py
@@ -12,7 +12,9 @@ with open('./ssh_cmd', 'w') as f:
 
 _repo_path = os.path.join('./', 'repo')
 # clone from remote
-git_repo = git.Repo.clone_from('gitclb@gitclb.git.backlog.com:/TEST/test.git', _repo_path, branch='master')
+ssh_executable = '../ssh_cmd'
+git_repo = git.Repo.clone_from('gitclb@gitclb.git.backlog.com:/TEST/test.git', _repo_path, branch='master').git.custom_environment(GIT_SSH=ssh_executable)
+# git_repo = git.Repo.clone_from('gitclb@gitclb.git.backlog.com:/TEST/test.git', _repo_path, branch='master')
 
 # create future branch
 origin = git_repo.remote()
