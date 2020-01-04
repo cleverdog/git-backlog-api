aa3ee48663db0f95248d6525e56ead743a0cff83
cleverdog <motoki@cleverdog.me>
2020-01-04 15:58:57 +0900

    test 2

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def post_json():
     elif issue_type == "要望":
         cat = 'REQUEST'
     branch_name = 'feature/' + project_key + '_' + cat + '_' + str(key_id)
-    # _repo_path = os.path.join('./', 'repo')
+    _repo_path = os.path.join('./', 'repo')
     # clone from remote
     # git_repo = git.Repo.clone_from('gitclb@gitclb.git.backlog.com:/TEST/test.git', _repo_path, branch='master')
     # create future branch
