ff6a86f5c6c93b8cf5b591ece11c18159562548e
cleverdog <motoki@cleverdog.me>
2020-01-04 18:14:34 +0900

    test 11

--- index.py
+++ index.py
@@ -1,9 +1,18 @@
 import os
 import git
 
+# SSH鍵の書き出し
+with open('./id_rsa.pub', 'w') as f:
+    f.write('<SSH Key>')
+    os.chmod('./id_rsa.pub', 0o600)
+# SSHコマンドの書き出し
+with open('./ssh_cmd', 'w') as f:
+    f.write('ssh -i ../id_rsa.pub -oIdentitiesOnly=yes "$@"')
+    os.chmod('./ssh_cmd', 0o777)
+
 _repo_path = os.path.join('./', 'repo')
 # clone from remote
-git_repo = git.Repo.clone_from('https://motoki@cleverdog.me@gitclb.backlog.com/git/TEST/test.git', _repo_path, branch='master')
+git_repo = git.Repo.clone_from('gitclb@gitclb.git.backlog.com:/TEST/test.git', _repo_path, branch='master')
 
 # create future branch
 origin = git_repo.remote()
